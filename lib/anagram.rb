38ad86018e5e43098c7a23f96613f11daaf899cf
Eric Storey <estorey@gmail.com>
2019-03-23 21:54:08 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,12 @@ class Anagram
   
   def match (possible_matches)
     matches=[]
-    
+    possible_matches.each{|match_word|
+      if match_word.split(//).sort==self.word.split(//).sort
+        matches << match_word
+      end
+    }
+    matches
   end
     
 end

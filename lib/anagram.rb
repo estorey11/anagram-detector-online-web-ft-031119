ed163bf4fe8ed7b54ba5e9d7fe17e4af99328e6b
Eric Storey <estorey@gmail.com>
2019-03-23 21:49:38 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+
+
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    self.word=word
+  end
+  
+  def match (possible_matches)
+    matches=[]
+    
+  end
+    
+end
